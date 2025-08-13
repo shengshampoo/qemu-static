058d8cb316b371822c76ed1bc86de6b266647d45
shengshampoo <shengzh@tutanota.com>
2025-08-13 11:44:19 +0000

    Update build-static-lib.sh

--- build-static-lib.sh
+++ build-static-lib.sh
@@ -14,16 +14,16 @@ ninja -C build install
 
 #libusb
 cd $WORKSPACE
-aria2c -x2 -R https://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.27/libusb-1.0.27.tar.bz2
-tar -xjf libusb-1.0.27.tar.bz2
-cd libusb-1.0.27
+aria2c -x2 -R https://downloads.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.29/libusb-1.0.29.tar.bz2
+tar -xjf libusb-1.0.29.tar.bz2
+cd libusb-1.0.29
 mkdir -p ./build ./build2
 cd build
 ../configure
 make -j8
 make install
 #libusb static
-cd $WORKSPACE/libusb-1.0.27/build2
+cd $WORKSPACE/libusb-1.0.29/build2
 ../configure --enable-static --disable-shared
 make -j8
 make install
@@ -43,9 +43,9 @@ make install
 
 #usbredir
 cd $WORKSPACE
-aria2c -x2 -R https://www.spice-space.org/download/usbredir/usbredir-0.14.0.tar.xz
-tar -vxf usbredir-0.14.0.tar.xz
-cd usbredir-0.14.0
+aria2c -x2 -R https://www.spice-space.org/download/usbredir/usbredir-0.15.0.tar.xz
+tar -vxf usbredir-0.15.0.tar.xz
+cd usbredir-0.15.0
 mkdir build
 cd build
 meson setup --buildtype=release -Ddefault_library=static ..
@@ -55,9 +55,9 @@ ninja install
 
 #fuse
 cd $WORKSPACE
-aria2c -x2 -R https://github.com/libfuse/libfuse/releases/download/fuse-3.16.2/fuse-3.16.2.tar.gz
-tar -vxf fuse-3.16.2.tar.gz
-cd fuse-3.16.2
+aria2c -x2 -R https://github.com/libfuse/libfuse/releases/download/fuse-3.17.3/fuse-3.17.3.tar.gz
+tar -vxf fuse-3.17.3.tar.gz
+cd fuse-3.17.3
 mkdir build
 cd build
 meson setup --buildtype=release -Ddefault_library=static ..
@@ -67,9 +67,9 @@ ninja install
 
 #SDL2
 cd $WORKSPACE
-aria2c -x2 -R https://github.com/libsdl-org/SDL/releases/download/release-2.32.0/SDL2-2.32.0.tar.gz
-tar -vxf SDL2-2.32.0.tar.gz
-cd SDL2-2.32.0
+aria2c -x2 -R https://github.com/libsdl-org/SDL/releases/download/release-2.32.8/SDL2-2.32.8.tar.gz
+tar -vxf SDL2-2.32.8.tar.gz
+cd SDL2-2.32.8
 mkdir build
 cd build 
 ../configure --enable-static --disable-shared --enable-pulseaudio
@@ -78,9 +78,9 @@ make -j8 && make install
 
 #SDL2_image
 cd $WORKSPACE
-aria2c -x2 -R https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.5/SDL2_image-2.8.5.tar.gz
-tar -vxf SDL2_image-2.8.5.tar.gz
-cd SDL2_image-2.8.5
+aria2c -x2 -R https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.8/SDL2_image-2.8.8.tar.gz
+tar -vxf SDL2_image-2.8.8.tar.gz
+cd SDL2_image-2.8.8
 mkdir build
 cd build 
 ../configure --enable-static --disable-shared 
