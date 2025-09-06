3ac0380df15defc76f5e7bad4ef371c3d23d9fcb
shengshampoo <shengzh@tutanota.com>
2025-09-06 04:54:42 +0000

    Update build-static-lib.sh

--- build-static-lib.sh
+++ build-static-lib.sh
@@ -25,6 +25,16 @@ ninja install
 #make -j8
 #make install
 
+# libjpeg-turbo
+cd $WORKSPACE
+git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git 
+cd libjpeg-turbo 
+mkdir build 
+cd build 
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=/usr -DWITH_JPEG8=1 -DENABLE_SHARED=false -DENABLE_STATIC=true .. 
+ninja 
+ninja install 
+
 
 #glib
 cd $WORKSPACE
@@ -34,7 +44,7 @@ cd glib-2.85.4
 mkdir build
 cd build
 #LDFLAGS='-lblkid -lmount -luuid -leconf' meson setup --buildtype=release -Ddefault_library=static -Dtests=false ..
-LDFLAGS='-leconf' meson setup --buildtype=release -Ddefault_library=static -Dtests=false ..
+LDFLAGS='-leconf' meson setup --buildtype=release -Ddefault_library=static -Dtests=false -Dprefix=/usr ..
 ninja
 ninja install
 
@@ -49,7 +59,7 @@ curl -sL https://gitlab.alpinelinux.org/alpine/aports/-/raw/master/main/fuse3/mo
 curl -sL https://gitlab.alpinelinux.org/alpine/aports/-/raw/master/main/fuse3/workaround-the-lack-of-support-for-rename2-in-musl.patch | patch -p1
 mkdir build
 cd build
-meson setup --buildtype=release -Ddefault_library=static ..
+meson setup --buildtype=release -Ddefault_library=static -Dprefix=/usr ..
 ninja
 ninja install
 
@@ -59,7 +69,7 @@ cd $WORKSPACE
 aria2c -x2 -R https://gitlab.freedesktop.org/slirp/libslirp/-/archive/master/libslirp-master.tar.bz2
 tar -vxf libslirp-master.tar.bz2
 cd libslirp-master
-meson setup --default-library static build 
+meson setup --default-library static -Dprefix=/usr build 
 ninja -C build install 
 
 #libusb
@@ -69,12 +79,12 @@ tar -xjf libusb-1.0.29.tar.bz2
 cd libusb-1.0.29
 mkdir -p ./build ./build2
 cd build
-../configure
+../configure --prefix=/usr
 make -j8
 make install
 #libusb static
 cd $WORKSPACE/libusb-1.0.29/build2
-../configure --enable-static --disable-shared
+../configure --enable-static --disable-shared --prefix=/usr
 make -j8
 make install
 
@@ -86,7 +96,7 @@ tar -vxf libusb-compat-0.1.7.tar.bz2
 cd libusb-compat-0.1.7
 mkdir build 
 cd build
-../configure --enable-static --disable-shared
+../configure --enable-static --disable-shared --prefix=/usr
 make -j8
 make install
 
@@ -99,7 +109,7 @@ cd usbredir-0.15.0
 mkdir build
 cd build
 #LDFLAGS='-lblkid -lmount -luuid -leconf' meson setup --buildtype=release -Ddefault_library=static ..
-LDFLAGS='-leconf' meson setup --buildtype=release -Ddefault_library=static ..
+LDFLAGS='-leconf' meson setup --buildtype=release -Ddefault_library=static -Dprefix=/usr ..
 ninja
 ninja install
 
@@ -111,7 +121,7 @@ tar -vxf SDL2-2.32.8.tar.gz
 cd SDL2-2.32.8
 mkdir build
 cd build 
-../configure --enable-static --disable-shared --enable-pulseaudio
+../configure --enable-static --disable-shared --enable-pulseaudio --prefix=/usr
 make -j8 && make install
 
 
@@ -122,7 +132,7 @@ tar -vxf SDL2_image-2.8.8.tar.gz
 cd SDL2_image-2.8.8
 mkdir build
 cd build 
-../configure --enable-static --disable-shared 
+../configure --enable-static --disable-shared --prefix=/usr
 make -j8 && make install
 
 #vnc
@@ -132,7 +142,7 @@ tar -vxf libvncserver-LibVNCServer-0.9.15.tar.gz
 cd libvncserver-LibVNCServer-0.9.15
 mkdir build
 cd build
-cmake .. -G Ninja -DBUILD_SHARED_LIBS=OFF
+cmake .. -G Ninja -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 ninja && DESTDIR=/ ninja install
 
 # dtc libfdt
